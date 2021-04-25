3a261ef25e50fdb67839da0e114bf85858fef1b8
George Lewis <georgejeffreylewis@gmail.com>
2021-04-25 13:38:45 -0400

    pylint

--- source/main.py
+++ source/main.py
@@ -1,22 +1,29 @@
+
+"""A discord bot that fixes Twitter video embeds"""
+
 import os
 import re
 
 import discord
-from crayons import *
+from crayons import blue, green, yellow, red
 from youtube_dl import YoutubeDL
 from youtube_dl.utils import DownloadError, ExtractorError
 
 
 def cprint(string: str, color):
+    """Print with a color"""
     print(color(string))
 
 
-def token():
+def token() -> str:
+    """Get the Discord token"""
     with open("discord_token.txt") as file:
         return file.read()
 
 
+# pylint: disable=missing-function-docstring
 class SilentLogger:
+    """Ignores logging"""
     def debug(self, msg):
         pass
 
@@ -35,6 +42,7 @@ cache = {filename.split(".")[0] for filename in os.listdir("videos") or []}
 
 
 class TwitVideo(discord.Client):
+    """Our bot"""
     async def on_ready(self):
         print(f"Logged in as {blue(self.user)}")
 
@@ -59,11 +67,11 @@ class TwitVideo(discord.Client):
                 ydl.download([link])
                 cprint("DOWNLOAD", yellow)
                 cache.add(status)
-            except (DownloadError, ExtractorError) as e:
-                cprint(f"SKIP: {e}", red)
+            except (DownloadError, ExtractorError) as error:
+                cprint(f"SKIP: {error}", red)
                 return
-            except Exception as e:
-                cprint(f"ERROR: {e}")
+            except Exception as error: # pylint: disable=broad-except
+                cprint(f"ERROR: {error}", red)
                 return
 
         with open(f"videos/{status}.mp4", "rb") as file:
